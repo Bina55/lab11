5ec04776670ab931f800efb7e346d209fffb46a3
Sabrina <sabrina.tassinari@studio.unibo.it>
2024-12-29 12:05:05 +0100

    Exercise 112 done

--- 112-stream-utilities-for-text-processing/src/main/java/it/unibo/oop/lab/streams/LambdaFilter.java
+++ 112-stream-utilities-for-text-processing/src/main/java/it/unibo/oop/lab/streams/LambdaFilter.java
@@ -6,7 +6,9 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.LayoutManager;
 import java.awt.Toolkit;
+import java.util.Arrays;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -33,12 +35,25 @@ import javax.swing.JTextArea;
 public final class LambdaFilter extends JFrame {
 
     private static final long serialVersionUID = 1760990730218643730L;
+    private static final String ANY_NON_WORD = "(\\s|\\p{Punct})+";
 
     private enum Command {
         /**
          * Commands.
          */
-        IDENTITY("No modifications", Function.identity());
+        IDENTITY("No modifications", Function.identity()),
+        LOWERCASE("Convert to lower case", s -> s.toLowerCase()),
+        COUNT_CHAR("Count the number of chars", s -> Integer.toString(s.length())),
+        COUNT_LINES("Count the number of lines", s -> Integer.toString((int)s.lines().count())),
+        ORDER("List all the word in alphabetical order", s -> 
+              Arrays.stream(s.split(ANY_NON_WORD)).sorted().collect(Collectors.joining("\n"))
+        ),
+        COUNT_WORD("Write the count for each word", s -> 
+            Arrays.stream(s.split(ANY_NON_WORD)).
+            collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).
+            entrySet().stream().map(t -> t.getKey() + " -> " + t.getValue()).
+            collect(Collectors.joining("\n"))
+        );
 
         private final String commandName;
         private final Function<String, String> fun;
@@ -84,6 +99,7 @@ public final class LambdaFilter extends JFrame {
         final int sh = (int) screen.getHeight();
         setSize(sw / 4, sh / 4);
         setLocationByPlatform(true);
+        
     }
 
     /**
